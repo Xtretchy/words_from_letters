3abc69d2a12402ce63d1b020b2572cc34f7872fe
Xtretchy <pelumisoyombo@gmail.com>
2022-07-23 22:10:27 +0200

    Modified main.py, and README.md files

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ A python program that gets all (or most) English words from a text
 - main.py: main python program
 - dictionary.txt: a list of basic English words, total of 45,333 words.
 - words_alpha.txt: a list of more English words, total of 370,104 words.
+
+To get more words, using the words_alpha.txt file, uncomment the second line containing the open() command.
--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-'''
-Program: words_from_letters.py
-Author(s): Oluwapelumi Soyombo
-Date: 16.08.2020
-'''
-
+# Date: 16.08.2020
 import itertools
 
 print('Program that gives you words (greater than 2) from your letters.\n')
@@ -11,13 +6,11 @@ print('Program that gives you words (greater than 2) from your letters.\n')
 file = open('dictionary.txt').read().lower() # dictionary for regular words
 #file = open('words_alpha.txt').read() # gives more (insane) words
 
-# collects input of letters from user,
-# creates a list to store new words.
+# collects input of letters from user, creates a list to store new words.
 letters = input('Enter the letters (as one word): ').lower()
 lst = []
 
-# uses a library to find all permutations
-# uses a loop to select sub-words as new permutation is made
+# uses a library to find all permutations, uses a loop to select sub-words as new permutation is made
 for i in itertools.permutations(letters):
     select = 3 # minumum number of words to find
     word = ''.join(i)
